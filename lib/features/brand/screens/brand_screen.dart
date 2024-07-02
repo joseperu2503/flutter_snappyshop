59f32b8ae393f2d699ca3d8c9af2cc2638a5d5e0
joseperu2503 <joseperu2503@gmail.com>
2024-07-02 00:52:04 -0500

    Update brand_screen.dart

--- lib/features/brand/screens/brand_screen.dart
+++ lib/features/brand/screens/brand_screen.dart
@@ -76,6 +76,12 @@ class BrandScreenState extends ConsumerState<BrandScreen> {
     super.dispose();
   }
 
+  double get widthProductCard {
+    final MediaQueryData screen = MediaQuery.of(context);
+
+    return (screen.size.width - 2 * 24 - 16) / 2;
+  }
+
   @override
   Widget build(BuildContext context) {
     final brand = ref
@@ -103,11 +109,11 @@ class BrandScreenState extends ConsumerState<BrandScreen> {
                 },
                 childCount: products.length,
               ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                crossAxisSpacing: 15,
-                mainAxisSpacing: 20,
-                mainAxisExtent: 210,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 12,
+                mainAxisExtent: widthProductCard + 80,
               ),
             ),
           ),
